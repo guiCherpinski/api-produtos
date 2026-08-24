eb268fc6da3069adf6467fd649babce5f46d2a6c
GUILHERME GONCALVES CHERPINSKI <guilherme_cherpin@estudante.sesisenai.org.br>
2026-08-24 16:18:24 -0300

    firts commit

---
+++ api-produtos/src/test/java/br/com/aula/api_produtos/ApiProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aula.api_produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
